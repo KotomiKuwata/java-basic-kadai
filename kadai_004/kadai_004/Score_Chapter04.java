f63dc557d5b36c8b40f588c77e3250f644809f90
KotomiKuwata <163006489+KotomiKuwata@users.noreply.github.com>
2024-05-08 20:48:02 +0900

    kadai_004をアップロード

---
+++ kadai_004/kadai_004/Score_Chapter04.java
@@ -0,0 +1,18 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+	public static void main(String[] args) {
+		System.out.println("Aさんは" + 10 + "点");
+		System.out.println("Bさんは" + 20 + "点");
+		System.out.println("Cさんは" + 30 + "点");
+		System.out.println("Dさんは" + 40 + "点");
+		System.out.println("Eさんは" + 50 + "点");
+		System.out.println("Fさんは" + 60 + "点");
+		System.out.println("Gさんは" + 70 + "点");
+		System.out.println("Hさんは" + 80 + "点");
+		System.out.println("Iさんは" + 90 + "点");
+		System.out.println("Jさんは" + 100 + "点");
+		System.out.println("平気点は" + ((10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90+ 100) / 10) + "点");
+	}
+
+}
