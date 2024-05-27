7dfb47013ed021b2e2cd814aa04d025ea421b505
KotomiKuwata <163006489+KotomiKuwata@users.noreply.github.com>
2024-05-27 12:12:51 +0900

    kadai_017をアップロード

---
+++ kadai_017/java_study/KatoExec_Chapter17.java
@@ -0,0 +1,28 @@
+package java_study;
+
+public class KatoExec_Chapter17 {
+
+	public static void main(String[] args) {
+		//各子クラスのインスタンス作成
+		KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
+		
+		KatoIchiro_Chapter17 ichiro = new KatoIchiro_Chapter17();
+		
+		KatoHanako_Chapter17 hanako = new KatoHanako_Chapter17();
+		
+		
+		taro.commonIntroduce();
+		taro.setGivenName();
+		taro.eachIntroduce();
+		
+		ichiro.commonIntroduce();
+		ichiro.setGivenName();
+		ichiro.eachIntroduce();
+		
+		hanako.commonIntroduce();
+		hanako.setGivenName();
+		hanako.eachIntroduce();
+		
+	}
+
+}
