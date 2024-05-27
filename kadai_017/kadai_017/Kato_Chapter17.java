1a3bca36c03a1c0b8a336fec5491f82a1e05b532
KotomiKuwata <163006489+KotomiKuwata@users.noreply.github.com>
2024-05-27 12:16:07 +0900

    kakdai_017をアップロード

---
+++ kadai_017/kadai_017/Kato_Chapter17.java
@@ -0,0 +1,18 @@
+package kadai_017;
+
+abstract public class Kato_Chapter17 { //親クラス
+	String familyName; //性
+	String givenName; //名
+	String address; //住所
+	
+	public void commonIntroduce() {//共通の紹介を出力 住所　名前
+		familyName =  "加藤";
+		address = "東京都中野区〇×";
+	}
+	
+	abstract public void eachIntroduce(); // 個別の紹介を出力する　特技　好きな食べ物　趣味
+	
+	public void execIntroduce() { //紹介を実行する
+		
+	}
+}
